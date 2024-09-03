57b605fe5de0cc36dd9f7d523c6bf66c712e8eaa
pengjunlee <pengjunlee@163.com>
2024-09-03 13:03:17 +0800

    5

--- src/components/GWLinkCard.vue
+++ src/components/GWLinkCard.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="link-card-root">
-    <div class="link-card" style="width:100%;">
-      <div class="name" :style="`fontSize: ${tsize}px`">
+    <div class="link-card" style="width: 100%">
+      <div class="name" :style="{ fontSize: tsize + 'px' }">
         {{ title }}
       </div>
       <div class="link-items">
-        <a :href="linkItem.url" target="_blank" class="el-card is-hover-shadow link-item" v-for="linkItem in items" :key="linkItem.url">
-          <img :src="linkItem.logo" alt="Logo" class="img" />
+        <a
+          :href="linkItem.url"
+          target="_blank"
+          class="el-card is-hover-shadow link-item"
+          v-for="linkItem in items"
+          :key="linkItem.url"
+        >
+          <img
+            v-if="linkItem.logo.startsWith('http')"
+            :src="linkItem.logo"
+            alt="Logo"
+            class="img"
+          />
+          <img v-else :src="`/icons/${linkItem.logo}`" alt="Logo" class="img" />
           <div class="link-text">
             <div class="nowrap link-title">{{ linkItem.title }}</div>
-            <div class="nowrap" style="font-size: 12px;">{{ linkItem.desc }}</div>
+            <div class="nowrap" style="font-size: 14px">
+              {{ linkItem.desc }}
+            </div>
+            <span class="tooltip-text">{{ linkItem.desc }}</span>
           </div>
         </a>
       </div>
@@ -18,42 +33,40 @@
 </template>
 
 <script setup lang="ts">
-import { ref, shallowRef, watch, onMounted, onActivated, provide, inject, nextTick } from "vue";
-import type { CardLinkItem } from '@/types/gw.props';
+import type { CardLinkItem } from "@/types/gw.props";
 import { toRefs } from "vue";
 
 interface ItemProps {
   title?: String;
-  tsize?: Number;
+  tsize?: number;
   items?: CardLinkItem[];
   isize?: Number;
 }
 
 const props = defineProps<ItemProps>();
 
-const {title, tsize, items, isize } = toRefs(props);
-
+const { title, tsize, items } = toRefs(props);
 </script>
 
 <style scoped lang="scss">
 .link-card-root {
-  @include box(100%, 100%);
+  width: 100%;
 }
 .link-items {
-    display: grid;
-    gap: 20px;
-    grid-template-columns: repeat(auto-fill, minmax(200px,1fr));
-    @include box(100%, 100%);
-    .link-item {
-      height: 100px;
-      padding: 10px;
-      border: 1px solid var(--gw-font-color-1);
-      border-radius: 5px;
-      display: flex;
-      align-items: center;
-      text-decoration: none;
-      color:var(--gw-font-color);
-      .img {
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  @include box(100%, 100%);
+  .link-item {
+    height: 100px;
+    padding: 10px;
+    border: 1px solid var(--gw-font-color-1);
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: var(--gw-font-color);
+    .img {
       width: 50px;
       height: 50px;
       border-radius: 5px;
@@ -61,43 +74,58 @@ const {title, tsize, items, isize } = toRefs(props);
       background-repeat: no-repeat;
       background-position: center;
       display: inline-block;
-     }
-     .link-text {
+    }
+    .link-text {
       font-family: ChillDuanSans_Light;
       display: flex;
       padding-left: 10px;
       flex-direction: column;
       flex-wrap: nowrap;
       align-items: flex-start;
-      max-width: 60px;
-     }
-    }
-    .link-item:hover {
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-      scale: 1.05;
+      font-size: 14px;
+      max-width: 150px;
     }
   }
-  .nowrap {
-  width: 80px;
-  text-overflow:ellipsis;
-  text-wrap:nowrap;
-  white-space-collapse:collapse;
+  .link-item:hover {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    scale: 1.05;
+  }
+}
+.nowrap {
+  width: 150px;
+  text-overflow: ellipsis;
+  text-wrap: nowrap;
+  white-space-collapse: collapse;
   overflow: hidden;
 }
 
 .name {
-  margin:15px 0;
+  margin: 15px 0;
   font-family: BaoTuXiaoBai;
-  font-weight:400;
-  ::after {
-    content: "";
-    background-color: aqua;
-    height: 30px;
-    width: 40px;
-  }
 }
 .link-title {
   font-family: ChillDuanSans_Black;
-  font-weight:400;
+  font-weight: 400;
+}
+
+.tooltip-text {
+  visibility: hidden; /* 初始隐藏 */
+  background-color: var(--gw-bg-active-color);
+  color: var(--gw-font-color);
+  text-align: center;
+  border-radius: 5px;
+  padding: 5px 10px;
+  position: absolute;
+  z-index: 1;
+  bottom: 0; /* 位于目标元素的上方 */
+  left: 50%;
+  transform: translateX(-50%);
+  opacity: 0; /* 设置为透明 */
+  transition: opacity 0.3s; /* 添加动画过渡效果 */
+}
+
+.link-text:hover .tooltip-text {
+  visibility: visible; /* 当悬停时显示 */
+  opacity: 1; /* 设置为不透明 */
 }
 </style>
